951475ed8815071ed66da02e202dcaf9801bd0c0
unknown <zyad.elkhadir@gmail.com>
2018-11-17 15:46:06 +0100

    Modify db file

--- Models/db.php
+++ Models/db.php
@@ -5,6 +5,8 @@ define('USER', 'root');
 
 define('PASS', 'root');
 
+define('DBNAME', 'shops');
+
 class db{
 
 public $db;
@@ -12,7 +14,7 @@ public $db;
 	function __construct(){
 
 
-		$this->db=new PDO("mysql:host=localhost;dbname=shops", USER, PASS);
+		$this->db=new PDO("mysql:host=localhost;dbname=".DBNAME, USER, PASS);
     // set the PDO error mode to exception
     $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
